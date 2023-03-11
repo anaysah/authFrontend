fda3d601ecd5d8f5969bf53d14bd14fe4b7cd69c
anaysah <ajaysahu0606@gmail.com>
2023-03-11 11:27:33 +0530

    added menu and header, transition

--- indes.js
+++ indes.js
@@ -1,35 +1,44 @@
-var spans = document.querySelectorAll(".menuButton");
+var spans = document.querySelectorAll(".auth-nav-Btn");
+var bgColor = "#2a2b38"
+
+const child1 = document.querySelector('#loginBox');
+const child2 = document.querySelector('#signupBox');
+
+child1.classList.toggle('show');
 
 for (var i = 0; i < spans.length; i++) {
-  spans[i].addEventListener("click", function(event) {
+  spans[i].addEventListener("click", function (event) {
     // Code to execute when the span is clicked
     // alert("You clicked span number " + (i+1));
 
     event.target.style.backgroundColor = "#1C0D1B";
     event.target.style.filter = "blur(0px)";
     // event.target.style.transform = "scale(0.8)";
-    
+
     let button = event.target.innerText;
 
 
-    if(button=="Sign Up"){
-        var otherBtn = document.querySelector("#loginBtn");
-        var mainBox = document.querySelector("#signupBox");
-        var otherBox = document.querySelector("#loginBox");
-        
-    }else if(button=="Log In"){
-        var otherBtn = document.querySelector("#signupBtn");
-        var mainBox = document.querySelector("#loginBox");
-        var otherBox = document.querySelector("#signupBox");
-        // otherBtn.style.backgroundColor = "#2a2b38";
+    if (button == "Sign Up") {
+      var otherBtn = document.querySelector("#auth-nav-loginBtn");
+      var mainBox = document.querySelector("#signupBox");
+      var otherBox = document.querySelector("#loginBox");
+
+    } else if (button == "Log In") {
+      var otherBtn = document.querySelector("#auth-nav-signupBtn");
+      var mainBox = document.querySelector("#loginBox");
+      var otherBox = document.querySelector("#signupBox");
+      // otherBtn.style.backgroundColor = "#2a2b38";
     }
 
     mainBox.style.display = "block";
     otherBox.style.display = "none";
     console.log(otherBtn)
-    otherBtn.style.backgroundColor = "#2a2b38";
+    otherBtn.style.backgroundColor = bgColor;
     otherBtn.style.filter = "blur(2px)";
-    
+
+    mainBox.classList.toggle('show')
+    otherBox.classList.toggle('show')
+
     // otherBtn.classList.add("blur"); /* Add the "blur" class to the div */
 
   });
